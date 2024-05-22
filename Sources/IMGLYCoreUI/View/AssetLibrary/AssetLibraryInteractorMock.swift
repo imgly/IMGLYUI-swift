1da741031ea85e7839890d2bbce3e774d90ed288
Intergalactic Igor <malte.baumann+bot@img.ly>
2024-05-22 09:43:27 +0000

    IMGLYUI version 1.26.1-rc.0

--- Sources/IMGLYCoreUI/View/AssetLibrary/AssetLibraryInteractorMock.swift
+++ Sources/IMGLYCoreUI/View/AssetLibrary/AssetLibraryInteractorMock.swift
@@ -22,7 +22,7 @@ class AssetLibraryInteractorMock: ObservableObject {
       let engine = try await Engine(license: secrets.licenseKey)
       self.engine = engine
       try engine.scene.createVideo()
-      let basePath = "https://cdn.img.ly/packages/imgly/cesdk-engine/1.27.0/assets"
+      let basePath = "https://cdn.img.ly/packages/imgly/cesdk-engine/1.26.1-rc.0/assets"
       try engine.editor.setSettingString("basePath", value: basePath)
       async let loadDefault: () = engine.addDefaultAssetSources()
       async let loadDemo: () = engine.addDemoAssetSources(sceneMode: engine.scene.getMode(),
