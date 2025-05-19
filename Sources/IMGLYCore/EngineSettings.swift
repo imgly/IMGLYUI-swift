4c63e0b46bac6df7d545560e06909651292e699c
Intergalactic Igor <malte.baumann+bot@img.ly>
2025-05-19 10:32:54 +0000

    IMGLYUI version 1.51.0

--- Sources/IMGLYCore/EngineSettings.swift
+++ Sources/IMGLYCore/EngineSettings.swift
@@ -18,7 +18,7 @@ public struct EngineSettings: Sendable {
   public init(
     license: String,
     userID: String? = nil,
-    baseURL: URL = .init(string: "https://cdn.img.ly/packages/imgly/cesdk-engine/1.51.0-rc.5/assets")!
+    baseURL: URL = .init(string: "https://cdn.img.ly/packages/imgly/cesdk-engine/1.51.0/assets")!
   ) {
     self.license = license
     self.userID = userID
