9ebc6ea549dac9cf89dbc77cacdaa10f2bdfe287
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-01-28 15:18:53 +0000

    IMGLYUI version 1.68.0-rc.1

--- Sources/IMGLYCoreUI/Model/Labelable/MappedEnum/HorizontalAlignment.swift
+++ Sources/IMGLYCoreUI/Model/Labelable/MappedEnum/HorizontalAlignment.swift
@@ -4,12 +4,14 @@ import Foundation
   case left = "Left"
   case center = "Center"
   case right = "Right"
+  case auto = "Auto"
 
   @_spi(Internal) public var localizationValue: String.LocalizationValue {
     switch self {
     case .left: "ly_img_editor_sheet_format_text_alignment_horizontal_option_left"
     case .center: "ly_img_editor_sheet_format_text_alignment_horizontal_option_center"
     case .right: "ly_img_editor_sheet_format_text_alignment_horizontal_option_right"
+    case .auto: "ly_img_editor_sheet_format_text_alignment_horizontal_option_auto"
     }
   }
 
@@ -18,6 +20,7 @@ import Foundation
     case .left: "text.alignleft"
     case .center: "text.aligncenter"
     case .right: "text.alignright"
+    case .auto: "text.alignleft" // Auto uses left-align icon as fallback
     }
   }
 }
