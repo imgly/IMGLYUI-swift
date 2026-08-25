58400547cdde1515070686fe43c395c3a0186eb8
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-08-25 17:19:16 +0000

    IMGLYUI version 1.81.1-rc.0

--- Sources/IMGLYCore/EngineSettings.swift
+++ Sources/IMGLYCore/EngineSettings.swift
@@ -33,5 +33,5 @@ public struct EngineSettings: Sendable {
 
   /// The default base URL used when no `baseURL` is provided: the public CE.SDK CDN.
   private static let defaultBaseURL =
-    URL(string: "https://cdn.img.ly/packages/imgly/cesdk-swift/1.81.0/assets")!
+    URL(string: "https://cdn.img.ly/packages/imgly/cesdk-swift/1.81.1-rc.0/assets")!
 }
